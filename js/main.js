0aa8cdcc6d96329746bcca49abc664654bbf970b
Ludvi01 <ludvany.marci@gmail.com>
2020-06-17 18:09:33 +0200

    update site

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
 function calcAmount() {
     let price = 1000;
     let amountInput = document.querySelector("input[name='amount-input']");
-    let showAmount = document.querySelector("span.show-amount");
-
     let amountNumber = parseInt(amountInput.value);
-
     amountNumber = isNaN(amountNumber) ? 0 : amountNumber;
+    
+    showSumPrice(price, amountNumber);
+}
 
+function showSumPrice(price, amountNumber) {
     if (amountNumber > 10) {
         alert("Maximum 10 terméket vásárolhat!");
     } else if (amountNumber < 1) {
         alert("Minimum 1 terméket kell vásárolnia!");
     } else {
         let amount = amountNumber * price;
+        let showAmount = document.querySelector("span.show-amount");
         showAmount.innerHTML = amount;
     }
 }
-
