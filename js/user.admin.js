6e51914d04b75a2d1cbd90a7c441b3068117d059
Ludvi01 <ludvany.marci@gmail.com>
2020-07-30 11:51:29 +0200

    update

--- js/user.admin.js
+++ js/user.admin.js
@@ -34,17 +34,17 @@ document.querySelector("#updatebtn").addEventListener("click", startGetUsers)
 function tableMarkupFromObjectArray(obj) {
   let headers = `
   ${Object.keys(obj[0]).map((col) => {
-    return `<th>${col}</th>`
+    return `<th scope="col">${col}</th>`
   }).join('')}
   <th>Buttons</th>`
 
   let content = obj.map((row, idx) => {
     return `<tr>
       ${Object.values(row).map((head) => {
-      return `<td>${head}</td>`
+      return `<td><input name=${head} value=${head}></td>`
     }).join('')}
     <td><div class="btn-group" role="group">
-    <button type="button" class="btn btn-info" onclick="getInfo(this)">
+    <button type="button" class="btn btn-info" onclick="setRow(this)">
       <i class="far fa-edit"></i></button>
     <button type="button" class="btn btn-danger" onclick="delRow(this)">
       <i class="far fa-trash-alt"></i></button></div></td>
@@ -76,23 +76,56 @@ let id = tr.querySelector("td:first-child").innerHTML;
     resp => resp.json(),
     err => console.error(err)
   ).then(
-    data => {
-      startGetUsers();
-    }
+    data => startGetUsers()
   );
 }
 
 function createUser(btn) {
   let tr = btn.parentElement.parentElement;
   let data = getRowData(tr);
-  console.log(data);
+  delete data.id;
+  let fetchOptions = {
+    method: "POST",
+    mode: "cors",
+    cache: "no-cache",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  };
+  fetch(`${userURL}`, fetchOptions).then(
+    resp => resp.json(),
+    err => console.error(err)
+  ).then(
+    data => startGetUsers()
+  )  
 }
 
 function getRowData(tr) {
-  let inputs = tr.querySelectorAll=("input.form-control");
+  let inputs = tr.querySelectorAll("input");
   let data = {};
   for (let i = 0; i < inputs.length; i++) {
     data[inputs[i].name] = inputs[i].value;
   }
   return data;
+}
+
+function setRow(btn) {
+  let tr = btn.parentElement.parentElement.parentElement;
+  let data = getRowData(tr);
+  let fetchOptions = {
+    method: "PUT",
+    mode: "cors",
+    cache: "no-cache",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  };
+  fetch(`${userURL}/${data.id}`, fetchOptions).then(
+    resp => resp.json(),
+    err => console.error(err)
+  ).then(
+    data => startGetUsers()
+  )
 }
